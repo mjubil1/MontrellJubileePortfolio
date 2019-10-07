815e0102d544df6140e19856c0c34b052daf6bd9
Montrell Jubilee <mjubil1@students.towson.edu>
2019-10-06 21:40:51 -0400

    WIP landing page and real estate page

---
+++ client/src/components/NavigationBar/RealEstateNavigationBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class RealEstateNavigationBar extends Component {
+  render() {
+    return (
+         <header>
+            <nav >
+               <div className="menu-icon">
+                  <i className="fa fa-bars fa-2x"></i>
+               </div>
+               <div className="logo">
+                  Montrell Jubilee
+               </div>
+               <div className="menu" style={{ backgroundColor: "blue" }}>
+                  <ul>
+                    <li><a href="#education">PROPERTY SEARCH</a></li>
+                    <li><a href="#experience">MY RECENT LISTINGS</a></li>
+                    <li><a href="#about">ABOUT ME </a></li>
+                    <li><a href="#contact">CONTACT ME</a></li>
+                  </ul>
+               </div>
+            </nav>
+         </header>
+    );
+  } 
+}
+
+export default RealEstateNavigationBar;
